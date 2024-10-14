4826af0e3a0fa15046800a3adaf57193039a4046
Abdul Rehman <91366959+rehmanNRY@users.noreply.github.com>
2024-10-14 12:57:59 -0700

    Add files via upload

---
+++ app/phpmailer_smtp/test.php
@@ -0,0 +1,34 @@
+<?php
+include('smtp/PHPMailerAutoload.php');
+$toEmail = $_GET['email'];
+$otp = $_GET['otp'];
+echo smtp_mailer($toEmail,'verify email address at reesearchhub',$otp);
+function smtp_mailer($to,$subject, $msg){
+	$mail = new PHPMailer(); 
+	$mail->IsSMTP(); 
+	$mail->SMTPAuth = true; 
+	$mail->SMTPSecure = 'tls'; 
+	$mail->Host = "smtp.gmail.com";
+	$mail->Port = 587; 
+	$mail->IsHTML(true);
+	$mail->CharSet = 'UTF-8';
+	//$mail->SMTPDebug = 2; 
+	$mail->Username = "abdulrrehman01@gmail.com";
+	$mail->Password = "pmtz bjci jyby xqiq";
+	$mail->SetFrom("abdulrrehman01@gmail.com");
+	$mail->Subject = $subject;
+	$mail->Body =$msg;
+	$mail->AddAddress($to);
+	$mail->SMTPOptions=array('ssl'=>array(
+		'verify_peer'=>false,
+		'verify_peer_name'=>false,
+		'allow_self_signed'=>false
+	));
+	if(!$mail->Send()){
+		echo $mail->ErrorInfo;
+	}else{
+    header('Location: ../views/profile/verify_email.php');
+		return 'Sent';
+	}
+}
+?>
